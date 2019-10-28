f10de5be3010f9c65befb66db7be9aaeaf58681e
Jonas <jbuser@unca.edu>
2019-10-28 10:20:55 -0400

    created getLocationPermission which will need to be called in order to get permission for ACCESS FINE LOCATION and sets mLocationGratned to true

--- app/app/src/main/java/com/unca/android/uncacampusbreeze/MainActivity.java
+++ app/app/src/main/java/com/unca/android/uncacampusbreeze/MainActivity.java
@@ -1,20 +1,28 @@
 package com.unca.android.uncacampusbreeze;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import static com.unca.android.uncacampusbreeze.Constants.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
 import static com.unca.android.uncacampusbreeze.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean mLocationGranted = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    //This method check to see if the GPS is setting is enabled
+    //This method checks to see if the GPS is setting is enabled
     public boolean isMapsEnabled(){
         final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
 
@@ -62,6 +70,38 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+
+    //This method requests the location permission. In order to eventually get the location of the device.
+    //The result will be handled in another method (onRequestPermissionsResult())
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {//Check for ACCESS FINE LOCATION permission
+            mLocationGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        mLocationGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationGranted = true;
+                }
+            }
+        }
+    }
 
 
 
