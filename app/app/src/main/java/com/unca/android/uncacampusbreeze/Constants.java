d8dbd623b12ec60d31cfe99f0c16bcc5c5dea06a
Jonas <jbuser@unca.edu>
2019-10-28 10:12:20 -0400

    Created noGPSAllert() which prompts a dialog if the user does not have GPS enabled and directs them to the settings screen in which they have to enable GPS

---
+++ app/app/src/main/java/com/unca/android/uncacampusbreeze/Constants.java
@@ -0,0 +1,9 @@
+package com.unca.android.uncacampusbreeze;
+//This class keeps track of constants for this project
+public class Constants {
+
+    public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    public static final int ERROR_DIALOG_REQUEST = 9001;
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9002;
+    public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9003;
+}
--- app/app/src/main/java/com/unca/android/uncacampusbreeze/MainActivity.java
+++ app/app/src/main/java/com/unca/android/uncacampusbreeze/MainActivity.java
@@ -1,14 +1,18 @@
 package com.unca.android.uncacampusbreeze;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.gms.common.GoogleApiAvailability;
+
+import static com.unca.android.uncacampusbreeze.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +31,32 @@ public class MainActivity extends AppCompatActivity {
         //startActivity(startNewActivity);
     }
 
+    //This creates an Alert if the user does not have GPS function on, and directs them to the settings page which allows the user to enable GPS
+    private void noGpsAllert() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("This application requires GPS to work, you need to enable it...")
+                .setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent goToGPS = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(goToGPS, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+
+
+
 
     //This method check to see if the GPS is setting is enabled
     public boolean isMapsEnabled(){
         final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
 
         if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-
+            noGpsAllert();
             return false;
         }
         return true;
