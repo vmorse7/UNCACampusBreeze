a6ee98099fa44b86250d340e64b2b8a28838f7a8
Jonas <jbuser@unca.edu>
2019-10-28 10:05:23 -0400

    Created isMapsEnabled() which makes sure that the user has GPS services enabled on their GPS settings

--- app/app/src/main/java/com/unca/android/uncacampusbreeze/MainActivity.java
+++ app/app/src/main/java/com/unca/android/uncacampusbreeze/MainActivity.java
@@ -2,9 +2,13 @@ package com.unca.android.uncacampusbreeze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +18,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+
+
+
     public void sendToMessages(View view){
         //Intent startNewActivity = new Intent(this, nextScreen.class); IMPORTANT: replace nextScreen.class with main class
         //startActivity(startNewActivity);
     }
+
+
+    //This method check to see if the GPS is setting is enabled
+    public boolean isMapsEnabled(){
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+
+
 }
