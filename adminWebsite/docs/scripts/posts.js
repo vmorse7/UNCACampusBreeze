2306b74f514fde84694a019dcfb2a0d75ebc2711
tmorse7 <tristan.morse7@gmail.com>
2019-11-15 14:03:12 -0500

    Added JavaScript integration for making test posts

--- adminWebsite/docs/scripts/posts.js
+++ adminWebsite/docs/scripts/posts.js
@@ -39,3 +39,26 @@ Pseudocode:
 
 */
 
+var postStruct = '<div id="postsCont">'+'<div class="titleCont">'+'<div class="postTitle">'+'<p id="title">Title</p>'+'<p id="date">Time/Date</p>'+'</div>'+'</div>'+'<div class="content">'+'<div class="text">'+'<p id="postText"></p>'+'</div>'+'<div class="postActions">'+'<button id="remove">Remove</button>'+'</div>'+'</div>'+'</div>';
+
+document.getElementById("postCont").innerHTML = postStruct;
+
+/*
+function loadPosts(){
+    document.getElementById("postCont").innerHTML = postStruct;
+    
+    
+    
+  var postStruct = '<div id="postsCont">'+'<div class="titleCont">'+'<div class="postTitle">'+'<p id="title">Title</p>'+'<p id="date">Time/Date</p>'+'</div>'+'</div>'+'<div class="content">'+'<div class="text">'+'<p id="postText"></p>'+'</div>'+'<div class="postActions">'+'<button id="remove">Remove</button>'+'</div>'+'</div>'+'</div>';
+  var postListLength = 9;
+  var postName = "Wahoo";
+  var postDate = "10-15-20";
+  var postText = "Yahoo";
+  
+    for(i = 0; i < 4; i++){
+        
+        
+        
+    }
+  
+}*/
