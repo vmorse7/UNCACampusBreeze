32f0ed58524ff871ae7ed8e66c304a6617a7d59b
John M <14935426+johnrmuse@users.noreply.github.com>
2019-11-14 20:40:58 -0500

    skeleton code for app login. As for the NewUidTask, I'm switching over to Task API as per https://firebase.google.com/docs/functions/callable

--- app/app/src/main/java/com/unca/android/uncacampusbreeze/LoungeActivity.java
+++ app/app/src/main/java/com/unca/android/uncacampusbreeze/LoungeActivity.java
@@ -1,11 +1,16 @@
 package com.unca.android.uncacampusbreeze;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
-public class LoungeActivity extends AppCompatActivity {
+import com.google.firebase.functions.FirebaseFunctions;
+import com.unca.android.uncacampusbreeze.server.TokenService;
+
+public class LoungeActivity extends Activity {
 
     private static final String TAG = "LoungeActivity";
 
@@ -15,6 +20,19 @@ public class LoungeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lounge);
         Log.d(TAG, "onCreate() being called.");
 
+        Log.d(TAG, "Authorizing app with server...");
+
+        // first we read from a file for a id number
+        SharedPreferences credentialsSharedPref = getActivity().getSharedPreferences("com.unca.android.uncacampusbreeze.credentials", Context.MODE_PRIVATE);
+        String defaultUid = "NONE";
+        String uidFromCredentials = credentialsSharedPref.getString("uid", defaultUid);
+        if (uidFromCredentials == defaultUid) { // if the app instance has never recieved an id from  server
+            // request new uid from server
+//            new DownloadFilesTask().execute(url1, url2, url3);
+            new NewUidTask().execute();
+        } else {
+            // authenticate with server
+        }
 
         Log.d(TAG, "onCreate() has finished.");
     }
@@ -71,6 +89,34 @@ public class LoungeActivity extends AppCompatActivity {
 
 
         Log.d(TAG, "onDestroy() has finished.");
+    }
+
+    public Context getActivity() {
+        return LoungeActivity.this;
+    }
+
+    private class NewUidTask extends AsyncTask<Void, Void, String> {
+
+        private static final String TAG = "NewUidTask";
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            Log.d(TAG, "doInBackground() being called.");
+
+            String newUID;
+            FirebaseFunctions functions;
+            functions = FirebaseFunctions.getInstance();
+            
+
+            Log.d(TAG, "doInBackground() has finished.");
+            return newUID;
+        }
+
+        protected void onPostExecute(String newUid) {
+            Log.d(TAG, "onPostExecute() being called.");
+            Log.d(TAG, "I recieved the uid " + newUid + " from the server.");
+            Log.d(TAG, "onPostExecute() has finished.");
+        }
     }
 }
 
