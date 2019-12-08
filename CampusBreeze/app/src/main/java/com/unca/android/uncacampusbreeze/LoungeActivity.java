5bc0d1b05dc227799996f1a28d5d9dfbaed3e3b1
John M <14935426+johnrmuse@users.noreply.github.com>
2019-12-07 21:26:09 -0500

    Initial app, but refreshed.

---
+++ CampusBreeze/app/src/main/java/com/unca/android/uncacampusbreeze/LoungeActivity.java
@@ -0,0 +1,51 @@
+package com.unca.android.uncacampusbreeze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoungeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lounge);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+}
